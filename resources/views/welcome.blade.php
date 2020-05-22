c33927a8e7e187bbfb551ba50cffe421a7e08323
Chihyu <chihyu10@gmail.com>
2020-05-22 11:47:47 +0200

    working on databases

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Chihyu KahI
+                    Chihyu Kah
                 </div>
 
                 <div class="links">
